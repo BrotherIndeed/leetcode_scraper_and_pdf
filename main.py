04849ea6849d46447eb4f87a35816f6799def258
BrotherIndeed <69084012+BrotherIndeed@users.noreply.github.com>
2021-12-07 23:05:03 +0530

    added index and difficulty level

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def update_question_links(question_links):
 
 def main():
     f = open("leetcode.txt", "w")
-
+    index = open("leetcodeindex.txt", "w")
     question_links = []
     update_question_links(question_links)
     i = 1
@@ -26,15 +26,27 @@ def main():
             data['variables'] = {"titleSlug": each_question_link}
             r = requests.post('https://leetcode.com/graphql', json=data).json()
             soup = BeautifulSoup(r['data']['question']['content'], 'lxml')
+            difficulty = r['data']['question']['difficulty']
             title = r['data']['question']['title']
             question = soup.get_text()
             f.write(str(i))
             f.write(". ")
             f.write(title)
-            f.write("\n\n\n")
+            f.write(
+                "\n                                                                                                                                                                         ")
+            f.write(difficulty)
+            f.write("\n\n")
             f.write(question)
             f.write("\n")
             f.write("\n\n\n-----------------------------------------------\n\n\n")
+
+            index.write(str(i))
+            index.write(". ")
+            index.write(title)
+            index.write(
+                "\n                                                                                                                                                                         ")
+            index.write(difficulty)
+            index.write("\n")
             print(i, "\n")
             i = i + 1
 
@@ -46,30 +58,3 @@ def main():
 if __name__ == '__main__':
     print('\n')
     main()
-
-# open google chrome, navigate to leetcode problems page
-# click on inspect, navigate to dev console and paste the commands below.
-# copy question links and paste in text file named question_links.txt
-# go to next page and repeat till you have all the questions links.
-# put python script and question link file in same folder.
-# run and wait to compile to text file, the convert to pdf online.
-# var x = document.querySelectorAll("a");
-# var myarray = []
-# for (var i=0; i<x.length; i++){
-#   if(x[i].getAttribute('href').indexOf('problem') > -1 && x[i].getAttribute('href').indexOf('solution') == -1 )
-#   {
-#   var cleanlink = x[i].href;
-#   myarray.push([cleanlink]);
-#   }
-
-# };
-# function make_table() {
-#     var table = '<table><thead><th>Links</th></thead><tbody>';
-#    for (var i=0; i<myarray.length; i++) {
-#             table += '<tr><td>'+ myarray[i][0] ;
-#     };
-
-#     var w = window.open("");
-# w.document.write(table);
-# }
-# make_table()
